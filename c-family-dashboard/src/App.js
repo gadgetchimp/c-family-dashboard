457b5e81477e9d0a7b2c7d057a5dbb36eaaf9ca1
gadgetchimp <contact.alexchavez@gmail.com>
2025-04-26 12:56:18 -0700

    Add files via upload

---
+++ c-family-dashboard/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import CalendarView from "./components/CalendarView";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>C Family Dashboard</h1>
+      <CalendarView />
+    </div>
+  );
+}
+
+export default App;
